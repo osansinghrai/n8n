0be6eeff73df9e3d15804bb6d74d21c136780f94
osanbantawarai <osanbantawarai@gmail.com>
2025-09-26 11:55:26 +0545

    fix

--- src/controller/googlesheet/googlesheet.controller.js
+++ src/controller/googlesheet/googlesheet.controller.js
@@ -17,32 +17,46 @@ const googleSheetController = async (req, res) => {
         })
         const client = await auth.getClient();
         console.log("Service Account Email:", client.email);
-        const sheets = google.sheets({ version: "v4", auth: client });
 
-        const googleSheetResponse = await sheets.spreadsheets.create({
-            resource: {
-                properties: {
-                    title: "Test sheet"
-                }
+        // authorization check
+        client.authorize(function(err) {
+            if (err)
+            {
+                console.error("Error authorizing client:", err);
+                return res.status(401).json({ message: "Unauthorized" });
+            }
+            else
+            {
+                console.log("Client authorized successfully");
+                return res.status(200).json({ message: "Authorized successfully" });
             }
         })
-        const drive = google.drive({ version: "v3", auth: client });
-        await drive.permissions.create({
-            fileId: googleSheetResponse.data.spreadsheetId,
-            requestBody: {
-                role: "writer",
-                type: "anyone"
-            }
-        })
+        // const sheets = google.sheets({ version: "v4", auth: client });
 
-        const googleSheetId = googleSheetResponse.data.spreadsheetId;
-        console.log("Google sheet ID:", googleSheetId);
-        const googleSheetUrl = googleSheetService(googleSheetId);
-        res.status(201).json({ 
-            status: "Success",
-            message: "Google Sheet created successfully",
-            googleSheetUrl: googleSheetUrl
-         });
+        // const googleSheetResponse = await sheets.spreadsheets.create({
+        //     resource: {
+        //         properties: {
+        //             title: "Test sheet"
+        //         }
+        //     }
+        // })
+        // const drive = google.drive({ version: "v3", auth: client });
+        // await drive.permissions.create({
+        //     fileId: googleSheetResponse.data.spreadsheetId,
+        //     requestBody: {
+        //         role: "writer",
+        //         type: "anyone"
+        //     }
+        // })
+
+        // const googleSheetId = googleSheetResponse.data.spreadsheetId;
+        // console.log("Google sheet ID:", googleSheetId);
+        // const googleSheetUrl = googleSheetService(googleSheetId);
+        // res.status(201).json({ 
+        //     status: "Success",
+        //     message: "Google Sheet created successfully",
+        //     googleSheetUrl: googleSheetUrl
+        //  });
     }
     catch (error){
         console.error("Error accessing Google Sheets API:", error);
